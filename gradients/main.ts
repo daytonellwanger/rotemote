0f404dd84533ad9925d7b911839ee6bb1248dc43
Dayton Ellwanger <daytonellwanger@gmail.com>
2025-12-25 11:50:29 -1000

    Multi-color gradient

--- gradients/main.ts
+++ gradients/main.ts
@@ -15,6 +15,28 @@ function interpolateColor(start: RGB, end: RGB, p: number): RGB {
     };
 }
 
+const stops: { p: number; color: RGB }[] = [
+    { p: 0.0, color: { r: 15, g: 25, b: 60 } },
+    { p: 0.25, color: { r: 40, g: 70, b: 140 } },
+    { p: 0.45, color: { r: 90, g: 160, b: 200 } },
+    { p: 0.6, color: { r: 255, g: 220, b: 150 } },
+    { p: 0.75, color: { r: 255, g: 140, b: 60 } },
+    { p: 0.9, color: { r: 220, g: 70, b: 40 } },
+    { p: 1.0, color: { r: 120, g: 30, b: 60 } },
+];
+
+function getColor(p: number): RGB {
+    for (let i = 0; i < stops.length - 1; i++) {
+        const start = stops[i];
+        const end = stops[i + 1];
+        if (p >= start.p && p <= end.p) {
+            const localP = (p - start.p) / (end.p - start.p);
+            return interpolateColor(start.color, end.color, localP);
+        }
+    }
+    throw new Error('p out of bounds');
+}
+
 const height = 400;
 const width = 500;
 const canvas = createCanvas(width, height);
@@ -24,7 +46,7 @@ const startColor: RGB = { r: 15, g: 25, b: 60 };
 const endColor: RGB = { r: 90, g: 160, b: 200 };
 
 for (let y = 0; y < height; y++) {
-    const color = interpolateColor(startColor, endColor, y / height);
+    const color = getColor(y / height);
     ctx.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
     ctx.fillRect(0, y, width, 1);
 }
