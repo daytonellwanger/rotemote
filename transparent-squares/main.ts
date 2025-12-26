b99518139550bd802bbb3d96f053aec1d1e8c4c4
Dayton Ellwanger <daytonellwanger@gmail.com>
2025-12-26 09:43:00 -1000

    Include patterned tiling example

--- transparent-squares/main.ts
+++ transparent-squares/main.ts
@@ -12,6 +12,38 @@ function getRandomColor() {
     return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}44`;
 }
 
+function randomTile() {
+    const randomX = random.normal(width / 2, width / 2);
+    const randomY = random.normal(height / 2, height / 2);
+    const randomSize = random.normal(50, 20);
+    for (let i = 0; i < 7500; i++) {
+        const randX = randomX();
+        const randY = randomY();
+        const randWidth = randomSize();
+        const randHeight = randomSize();
+        ctx.fillStyle = getRandomColor();
+        ctx.fillRect(randX, randY, randWidth, randHeight);
+    }
+}
+
+const xStart = -100;
+const yStart = -100;
+function patternedTile() {
+    let x = xStart;
+    let y = yStart;
+    const squareSize = 50;
+    const randomStep = random.normal(squareSize / 5, squareSize / 6);
+    while (y < height) {
+        while (x < width) {
+            ctx.fillStyle = getRandomColor();
+            ctx.fillRect(x, y, squareSize, squareSize);
+            x += randomStep();
+        }
+        x = xStart;
+        y += randomStep();
+    }
+}
+
 const height = 800;
 const width = 450;
 const canvas = createCanvas(width, height);
@@ -20,17 +52,8 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, width, height);
 
-const randomX = random.normal(width / 2, width / 2);
-const randomY = random.normal(height / 2, height / 2);
-const randomSize = random.normal(50, 20);
-for (let i = 0; i < 7500; i++) {
-    const randX = randomX();
-    const randY = randomY();
-    const randWidth = randomSize();
-    const randHeight = randomSize();
-    ctx.fillStyle = getRandomColor();
-    ctx.fillRect(randX, randY, randWidth, randHeight);
-}
+// patternedTile();
+randomTile();
 
 const buffer = canvas.toBuffer('image/png');
 fs.writeFileSync('squares.png', buffer);
