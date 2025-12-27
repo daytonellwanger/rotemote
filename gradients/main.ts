44a61b07b461264cc34eef75730d4bbcc7a31898
Dayton Ellwanger <daytonellwanger@gmail.com>
2025-12-27 10:22:47 -1000

    More realistic gradient

--- gradients/main.ts
+++ gradients/main.ts
@@ -16,13 +16,11 @@ function interpolateColor(start: RGB, end: RGB, p: number): RGB {
 }
 
 const stops: { p: number; color: RGB }[] = [
-    { p: 0.0, color: { r: 15, g: 25, b: 60 } },
-    { p: 0.25, color: { r: 40, g: 70, b: 140 } },
-    { p: 0.45, color: { r: 90, g: 160, b: 200 } },
-    { p: 0.6, color: { r: 255, g: 220, b: 150 } },
-    { p: 0.75, color: { r: 255, g: 140, b: 60 } },
-    { p: 0.9, color: { r: 220, g: 70, b: 40 } },
-    { p: 1.0, color: { r: 120, g: 30, b: 60 } },
+    { p: 0.0, color: { r: 50, g: 63, b: 74 } },
+    { p: 0.25, color: { r: 143, g: 154, b: 153 } },
+    { p: 0.5, color: { r: 213, g: 167, b: 117 } },
+    { p: 0.8, color: { r: 255, g: 174, b: 56 } },
+    { p: 1.0, color: { r: 233, g: 129, b: 52 } },
 ];
 
 function getColor(p: number): RGB {
